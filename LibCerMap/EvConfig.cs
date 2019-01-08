4dd8ebcef2fed03bc66879fc3bc3bfb1c95a3438
wayelee2000@gmail.com <wayelee2000@gmail.com>
2019-01-08 11:37:01 +0800

    添加标准列名

--- LibCerMap/EvConfig.cs
+++ LibCerMap/EvConfig.cs
@@ -7,9 +7,22 @@ namespace LibCerMap
 {
     class EvConfig
     {
-        public static string CenterlineMeasureField = "里程（m）";
-        public static string CenterlineZField = "Z_高程（米";
-        public static string IMUMoveDistanceField = "记录距离__";
-         
+        public static string CenterlineXField = "经度";
+        public static string CenterlineYField = "纬度";
+        public static string CenterlineZField = "高程";
+        public static string CenterlineMeasureField = "里程";
+        public static string CenterlinePointTypeField = "测点属性";
+        
+        public static string IMUMoveDistanceField = "记录距离";
+        public static string IMUPointTypeField = "类型";
+        public static string IMULengthField = "长度";
+        public static string IMUWidththField = "宽度";
+        public static string IMUDepthField = "壁厚";
+        public static string IMUAngleField = "时钟方位";
+
+        public static string WeldXField = "经度";
+        public static string WeldYField = "纬度";
+        public static string WeldZField = "高程";
+        public static string WeldMField = "里程";
     }
 }
